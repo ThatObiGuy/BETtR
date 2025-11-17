5733b8edf9bde16c8b648d3018e9591ae0763c0a
SorinBivol <MATHSTATDS\22473462@rstudio.maths.nuim.ie>
2025-11-17 19:48:29 +0000

    author added + hello file added function

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -16,3 +16,9 @@
 hello <- function() {
   print("Hello, world!")
 }
+
+
+# to check if you added yourself as an author
+desc <- read.dcf("DESCRIPTION")
+desc[, "Authors@R"]
+
