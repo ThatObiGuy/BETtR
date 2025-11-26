2928fc3d6c79ca7f1d6f24b57e1903591eeb195a
Zala04 <zaladio135@example.com>
2025-11-26 15:43:16 +0000

    hecker if its bettr_data class

--- R/make_bettr.R
+++ R/make_bettr.R
@@ -1,5 +1,9 @@
 make_bettr <- function(data) {
+  class(data) <- c("bettr_data")
+
+
 
   ## Takes odds data in the form of a base r df and transforms it into a bettr_data object
 
 }
+
--- R/plot.bettr_data.R
+++ R/plot.bettr_data.R
@@ -1,4 +1,5 @@
 plot.bettr_data <- function(x, ...) {
+  if(!inherits(x, "bettr_data")) stop("x must be of class 'bettr_data'")
 
   ## takes an object with the bettr_data attribute plots odds movement over time as well as information gain over time. (Owen)
 
