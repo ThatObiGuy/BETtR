03fa0a894ba9ad939f6cc2c97c2e7264e4393a82
Zala04 <zaladio135@gmail.com>
2025-11-26 21:15:08 +0000

    commit adding time and odds (mostly to see if my git appears)

--- R/plot.bettr_data.R
+++ R/plot.bettr_data.R
@@ -2,5 +2,7 @@ plot.bettr_data <- function(x, ...) {
   if(!inherits(x, "bettr_data")) stop("x must be of class 'bettr_data'")
 
   ## takes an object with the bettr_data attribute plots odds movement over time as well as information gain over time. (Owen)
-
+  time = x$time
+  odds = x$odds
+  if(!is.numeric(time)) warning("needs to be a numerical input")
 }
