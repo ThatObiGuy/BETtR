5e1cd36d5d93ef458f11c9982a295aef4cd91a9c
Zala04 <zaladio135@gmail.com>
2025-12-04 23:05:38 +0000

    fixed tsibble dataset for our own package dataset, created the first prototype of make_bettr obj creator, reinitiated the tidyingfirstDataset

--- R/make_bettr.R
+++ R/make_bettr.R
@@ -1,9 +1,28 @@
-make_bettr <- function(data) {
-  class(data) <- c("bettr_data")
-
-
-
-  ## Takes odds data in the form of a base r df and transforms it into a bettr_data object
-
+make_bettr <- function(data,
+                       event_id = "event_id",
+                       logged_time = "logged_time",
+                       home_odds = "home_odds",
+                       away_odds = "away_odds",
+                       draw_odds = "draw_odds", ...) {
+  #checking if it has the 5 required columns to be a bettr object
+  req <- c(event_id, logged_time, home_odds, away_odds, draw_odds)
+  allReqNotInData <- setdiff(req, names(data))
+  if(length(allReqNotInData) != 0) {
+    stop("data input is missing the following columns: ",
+         paste(allReqNotInData, collapse = ", "))
+  }
+   numericCols <- c("home_odds", "away_odds", "draw_odds")
+  if(!all(sapply(data[numericCols], is.numeric))) {
+    stop("(home_odds, away_odds, draw_odds) must be numeric.")
+  }
+  if (any(is.na(as.POSIXct(data[[logged_time]], format = "%Y-%m-%d %H:%M:%S")))) {
+    stop("input must be in the format 'YYYY-MM-DD HH:MM:SS'.")
+  }
+  data_tibble <- data |>
+  as_tsibble(key = event_id, index = logged_time)
+  class(data_tibble) <- c("bettr_data", class(data_tibble))
+  return(data_tibble)
 }
+x1 <- epl_weekend |> make_bettr()
+class(x1)
 
--- inst/TidyingFirstDataset.R
+++ inst/TidyingFirstDataset.R
@@ -1,26 +1,55 @@
-library(dplyr)
-library(readr)
 
-epl_weekend_data <- read_csv("bettr/inst/epl_weekend_data.csv",
-                             col_types = cols(event_id = col_character(),
-                                              logged_time = col_datetime(format = "%Y-%m-%d %H:%M:%S"),
-                                              sport_id = col_skip(), league_id = col_skip(), league_id = col_character(),
-                                              starts = col_datetime(format = "%Y-%m-%d %H:%M:%S")))
+#library(dplyr)
+#epl_weekend_data <- read_csv("~/epl_weekend_data.csv")
+#epl_weekend_data
 
-epl_weekend_data |> group_by(event_id) |> arrange(logged_time,.by_group = TRUE) |> arrange(event_id, logged_time) -> epl_arranged
+#eplArranged <- epl_weekend_data |> group_by(event_id) |> arrange(logged_time
+   #,.by_group = TRUE) |> arrange(event_id, logged_time)
+#eplArranged
+#unique(eplArranged[,1])
+#eplArranged %>% select(event_id) %>% group_by(event_id) %>%  mutate(matchID = 1:10)
+#view(eplArranged)
+#vec <- c(1:10)
+#eplArranged %>%
+ # group_by(event_id)
+#unique(eplArranged$event_id)
+#view(eplArranged)
 
-epl_arranged %>% select(event_id) %>% group_by(event_id) %>%  mutate(matchID = 1:10)
+#mapping <- data.frame(
+ # old = c(1619454336, 1619454337, 1619454338, 1619454339, 1619454340, 1619454450,
+ #         1619454451 ,1619454452, 1619456908, 1619456909),
+ # new = c(1:length(unique(eplArranged$event_id)))
+#)
 
-mapping <- data.frame(
-  old = c(1619454336, 1619454337, 1619454338, 1619454339, 1619454340, 1619454450,
-          1619454451 ,1619454452, 1619456908, 1619456909),
-  new = c(1:length(unique(epl_arranged$event_id)))
-)
+#eplArranged$event_id <- mapping$new[match(eplArranged$event_id, mapping$old)]
+#view(eplArranged)
 
-epl_arranged$event_id <- mapping$new[match(epl_arranged$event_id, mapping$old)]
+#saveRDS(eplArranged, file = "OddsData1.rds")
+#readRDS(file = "OddsData1.rds")
+#save("OddsData1.rds")
+# Replace USERNAME with your actual username
+#epl_weekend <- readRDS("/Users/sorinbivol/Desktop/OddsData1.rds")
+#save(epl_weekend, file = "OddsRatio1.RData")
+#load("~/bettr/data/OddsRatio1.RData")
 
-epl_weekend <- epl_arranged
+load("~/bettr/data/OddsRatio1.RData")
+library(tsibble)
+colnames(epl_weekend)
+epl_weekend_bettr <- subset(epl_weekend, select = c(event_id, logged_time,
+                                                    home_odds, away_odds,
+                                                    draw_odds, home_team,
+                                                    away_team, max_money_line
+                                                    ,league_name))
+#epl_weekend_bettr
+epl_weekend_tsibble <- epl_weekend_bettr |>
+  as_tsibble(key = event_id, index = logged_time)
+epl_weekend_tsibble <- epl_weekend_tsibble |> ungroup()
+class(epl_weekend_tsibble) <- c("bettr_data", class(epl_weekend_tsibble))
 
-class(epl_weekend) <- c("bettr_data", "data.frame")
-  
-saveRDS(epl_arranged, file = "epl_weekend.rds")
+
+class(epl_weekend_tsibble)
+
+saveRDS(epl_weekend_tsibble, file = "epl_weekend_tsibble.rds")
+epl_weekend_tsibble <- readRDS("~/bettr/data/epl_weekend_tsibble.rds")
+
+
