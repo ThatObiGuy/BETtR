2bcafecda13b6db50cf78fb465a33535a659564d
Sean-OLeary <olearyseanjoshua04@gmail.com>
2025-12-04 15:18:15 +0000

    Added more text to start up message.
    Put StartUpMessage in function with temp params just to run roxygen

--- R/better-package.R
+++ R/better-package.R
@@ -1,1 +1,15 @@
-packageStartupMessage(paste("\n ____  _____ _____ _   ____  \n| __ )| ____|_   _| |_|  _ \\ \n|  _ \\|  _|   | | | __| |_) |\n| |_) | |___  | | | |_|  _ < \n|____/|_____| |_|  \\__|_| \\_\\ \t"))
+#' Title
+#'
+#' @param x
+#' @param y
+#' @param ...
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+.onAttach <- function(x,y,...){
+
+  packageStartupMessage(paste("\n ____  _____ _____ _   ____  \n| __ )| ____|_   _| |_|  _ \\      Calculate Shannon Entropy and\n|  _ \\|  _|   | | | __| |_) |     Provide Optimisation Functions\n| |_) | |___  | | | |_|  _ <      Version: 0.1.0\n|____/|_____| |_|  \\__|_| \\_\\ \t\n\n\nType", sQuote("?BETtR"), "to see a brief guide on how to use this R-Package."))
+
+}
