0f26e3bf70fb19cf71e6695fcd86a3981f86ccb8
Owen F. O'Connor <owen.oconnor.2024@mumail.ie>
2025-12-04 16:25:33 +0000

    Cleaning up data cleaning, increase reproducability by adding original CSV

--- inst/TidyingFirstDataset.R
+++ inst/TidyingFirstDataset.R
@@ -1,32 +1,26 @@
+library(dplyr)
+library(readr)
 
-#library(dplyr)
-#epl_weekend_data <- read_csv("~/epl_weekend_data.csv")
-#epl_weekend_data
+epl_weekend_data <- read_csv("bettr/inst/epl_weekend_data.csv",
+                             col_types = cols(event_id = col_character(),
+                                              logged_time = col_datetime(format = "%Y-%m-%d %H:%M:%S"),
+                                              sport_id = col_skip(), league_id = col_skip(), league_id = col_character(),
+                                              starts = col_datetime(format = "%Y-%m-%d %H:%M:%S")))
 
-#eplArranged <- epl_weekend_data |> group_by(event_id) |> arrange(logged_time
-   #,.by_group = TRUE) |> arrange(event_id, logged_time)
-#eplArranged
-#unique(eplArranged[,1])
-#eplArranged %>% select(event_id) %>% group_by(event_id) %>%  mutate(matchID = 1:10)
-#view(eplArranged)
-#vec <- c(1:10)
-#eplArranged %>%
- # group_by(event_id)
-#unique(eplArranged$event_id)
-#view(eplArranged)
+epl_weekend_data |> group_by(event_id) |> arrange(logged_time,.by_group = TRUE) |> arrange(event_id, logged_time) -> epl_arranged
 
-#mapping <- data.frame(
- # old = c(1619454336, 1619454337, 1619454338, 1619454339, 1619454340, 1619454450,
- #         1619454451 ,1619454452, 1619456908, 1619456909),
- # new = c(1:length(unique(eplArranged$event_id)))
-#)
+epl_arranged %>% select(event_id) %>% group_by(event_id) %>%  mutate(matchID = 1:10)
 
-#eplArranged$event_id <- mapping$new[match(eplArranged$event_id, mapping$old)]
-#view(eplArranged)
+mapping <- data.frame(
+  old = c(1619454336, 1619454337, 1619454338, 1619454339, 1619454340, 1619454450,
+          1619454451 ,1619454452, 1619456908, 1619456909),
+  new = c(1:length(unique(epl_arranged$event_id)))
+)
 
-#saveRDS(eplArranged, file = "OddsData1.rds")
-#readRDS(file = "OddsData1.rds")
-#save("OddsData1.rds")
-# Replace USERNAME with your actual username
+epl_arranged$event_id <- mapping$new[match(epl_arranged$event_id, mapping$old)]
 
+epl_weekend <- epl_arranged
 
+class(epl_weekend) <- c("bettr_data", "data.frame")
+  
+saveRDS(epl_arranged, file = "epl_weekend.rds")
