dc4497ba625b14d5effb16f5b42169b40427130b
Owen F. O'Connor <owen.oconnor.2024@mumail.ie>
2025-12-10 17:34:54 +0000

    Added global variables to combat warning and generated files for football data

---
+++ R/global_variables.R
@@ -0,0 +1,2 @@
+if(getRversion() >= "2.15.1") utils::globalVariables(c("event_id", "logged_time", "home_odds", "draw_odds", "away_odds", "fav_choice",
+                                                       "opening_fav_odds", "fav_odds", "location_adjusted_fav_odds", "scale_adjusted_fav_odds", "x", "year"))
