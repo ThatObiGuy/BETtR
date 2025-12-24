004936ddc2292c559e083268e2782323e00387e7
Owen F. O'Connor <owen.oconnor.2024@mumail.ie>
2025-12-24 18:15:46 +0000

    Ran Desc_normalise and iterated upon plot bettr - now you have choice of odds.
    Also ran spellcheck - nothing stood out to me as being incorrect - just european as opposed to american spellings.

--- R/plot.bettr_data.R
+++ R/plot.bettr_data.R
@@ -2,7 +2,10 @@
 #'
 #' Plots percentage change of the odds of a (or many) opening favorite(s) change over time.
 #'
-#' @param x An object of class \code{"bettr_data"} - You can use the inbuilt dataset, \code{"football"} or using your own odds data after using the \code{link{make_bettr}} function.
+#' @param x An object of class \code{"bettr_data"} - You can use the inbuilt dataset,
+#' \code{"football"} or using your own odds data after using the \code{link{make_bettr}} function.
+#' @param odd Either \code{"CF"} or \code{"OF"} for Closing Favorite or Opening Favorite respectively.
+#' Defaults to \code{"CF"}.
 #' @param ... Catches unused arguments to \code{plot} (not currently implemented).
 #'
 #' @returns a ggplot using geom_line to show the odds movement over time
@@ -15,38 +18,71 @@
 #'
 #' @examples
 #' plot(football)
-plot.bettr_data <- function(x, ...) {
-  if(!inherits(x, "bettr_data")) stop("x must be of class 'bettr_data'")
-  x <- dplyr::as_tibble(x) # fixing compatability with tstibble bettr_data
+plot.bettr_data <- function(x, odd = c("CF", "OF"), ...) {
 
-  # Identify opening favourites and their opening odds from given dataframe
+  # Checking x is of class bettr_data
+  if(!inherits(x, "bettr_data")) stop("x must be of class 'bettr_data'")
+
+  # Checking the given argument for the odd parameter is valid
+  odd <- match.arg(odd)
+
+  # Fixing compatability with tstibble bettr_data
+  x <- dplyr::as_tibble(x)
+
+  # We require an object containing opening odds
   x |>
     dplyr::group_by(event_id) |>
     dplyr::slice_min(logged_time, n = 1) |>
-    dplyr::mutate(fav_choice = c("home_odds", "draw_odds", "away_odds")[which.max(c(home_odds, draw_odds, away_odds))],
-                  opening_fav_odds = dplyr::case_when(
-                    fav_choice == "home_odds" ~ home_odds,
-                    fav_choice == "draw_odds" ~ draw_odds,
-                    fav_choice == "away_odds" ~ away_odds
-                  )) |>
-    dplyr::select(event_id, fav_choice, opening_fav_odds) -> x_opening_favourites
+    dplyr::select(event_id, home_odds, draw_odds, away_odds) -> opening_odds
 
-  # Appending original data with this info
+  # We must identify the opening or closing favourites based on user's argument
+  if (odd == "OF") {
+    opening_odds |>
+      dplyr::mutate(fav_choice = c("home_odds", "draw_odds", "away_odds")[which.max(c(home_odds, draw_odds, away_odds))],
+                    opening_fav_odds = dplyr::case_when(
+                      fav_choice == "home_odds" ~ home_odds,
+                      fav_choice == "draw_odds" ~ draw_odds,
+                      fav_choice == "away_odds" ~ away_odds
+                    )) |>
+      dplyr::select(event_id, fav_choice, opening_fav_odds) -> x_target_odd
+
+  } else if (odd == "CF") {
+    x |>
+      dplyr::group_by(event_id) |>
+      dplyr::slice_max(logged_time, n = 1) |>
+      dplyr::mutate(fav_choice = c("home_odds", "draw_odds", "away_odds")[which.max(c(home_odds, draw_odds, away_odds))]) |>
+      dplyr::select(event_id, fav_choice) -> x_target_odd
+
+    x_target_odd |>
+      dplyr::left_join(opening_odds, by = "event_id") |>
+      dplyr::mutate(opening_fav_odds = dplyr::case_when(
+                      fav_choice == "home_odds" ~ home_odds,
+                      fav_choice == "draw_odds" ~ draw_odds,
+                      fav_choice == "away_odds" ~ away_odds
+                    )) |>
+      dplyr::select(event_id, fav_choice, opening_fav_odds) -> x_target_odd
+
+  }
+
+  # Appending original data with this info and transforming data
   x |>
-    dplyr::left_join(x_opening_favourites, by = "event_id") |>
+    dplyr::left_join(x_target_odd, by = "event_id") |>
     dplyr::mutate(
       fav_odds = dplyr::case_when(
         fav_choice == "home_odds" ~ home_odds,
         fav_choice == "draw_odds" ~ draw_odds,
         fav_choice == "away_odds" ~ away_odds
       ),
-      location_adjusted_fav_odds = fav_odds - opening_fav_odds, # Bringing all odds to start at zero
-      scale_adjusted_fav_odds = location_adjusted_fav_odds / opening_fav_odds # Adjusting scale
-    ) -> x_plottable
+      pct_change_fav_odds = (fav_odds - opening_fav_odds) / opening_fav_odds * 100 ) |>
+    dplyr::select(event_id, logged_time, pct_change_fav_odds) -> x_plottable
   # Scale and location adjustments are so that we can focus on displaying movement on a similar scale with a fixed start.
-  # We could in future allow the user to specify scaling parameter
+  # A percentage change is a very reasonable scale
 
-  p <- ggplot2::ggplot(x_plottable, ggplot2::aes(x=logged_time, y=scale_adjusted_fav_odds, colour=event_id)) +
+  p <- ggplot2::ggplot(x_plottable, ggplot2::aes(x=logged_time, y=pct_change_fav_odds, colour=event_id)) +
+    ggplot2::theme_minimal() +
+    ggplot2::xlab("Time") +
+    ggplot2::ylab("Odds Change (%)") +
+    ggplot2::ggtitle(paste("Change over time of odds data (", odd, ")")) +
     ggplot2::geom_line()
 
   print(p)
