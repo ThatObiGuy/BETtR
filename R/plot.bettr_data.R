f080b6e4207f66a94d2b180589c4a3bae839c8d9
Owen F. O'Connor <owen.oconnor.2024@mumail.ie>
2025-12-28 21:52:03 +0000

    A column including match fixtures can now be provided as an optional additional argument. If this information is available it can make matches eaiser to identify from the graph than some obscure "event_id" - optional nature respects that fixture is not a mandatory column for bettr_data objects.

--- R/plot.bettr_data.R
+++ R/plot.bettr_data.R
@@ -7,6 +7,8 @@
 #' \code{"football"} or using your own odds data after using the \code{\link{make_bettr}} function.
 #' @param odd Either \code{"CF"} or \code{"OF"} for Closing Favorite or Opening Favorite respectively.
 #' Defaults to \code{"CF"}.
+#' @param fixture Optional additional column in \code{x} to use for tooltips
+#' and legend instead of \code{event_id}. Defaults to NULL (uses \code{event_id}).
 #' @param ... Additional arguments passed to \code{ggiraph::girafe()} for specifying dimensions of output.
 #'
 #' @returns a \code{ggiraph} object showing the odds movement over time
@@ -15,13 +17,18 @@
 #' @author Owen F. O'Connor - <\email{owen.oconnor.2024@@mumail.ie}>
 #'
 #' @importFrom dplyr "as_tibble" "group_by" "slice_min" "mutate" "case_when" "select" "left_join"
-#' @importFrom ggplot2 "ggplot" "geom_line"
+#' @importFrom ggplot2 "ggplot" "geom_line" "scale_color_discrete"
 #' @importFrom ggiraph "geom_line_interactive" "girafe"
 #'
 #' @examples
 #' plot(football)
 #' plot(football, odd = "CF", width_svg = 8)
-plot.bettr_data <- function(x, odd = c("CF", "OF"), ...) {
+#'
+#' football2 <- football |>
+#'   dplyr::mutate(fixture = paste(home_team, "vs", away_team))
+#'
+#' plot(football2, fixture = "fixture")
+plot.bettr_data <- function(x, odd = c("CF", "OF"), fixture = NULL, ...) {
 
   # Checking x is of class bettr_data
   if(!inherits(x, "bettr_data")) stop("x must be of class 'bettr_data'")
@@ -31,6 +38,13 @@ plot.bettr_data <- function(x, odd = c("CF", "OF"), ...) {
 
   # Fixing compatability with tstibble bettr_data
   x <- dplyr::as_tibble(x)
+
+  # Validate fixture column if provided
+  if (!is.null(fixture)) {
+    if (!fixture %in% names(x)) {
+      stop("Column '", fixture, "' not found in x")
+    }
+  }
 
   # We require an object containing opening odds
   x |>
@@ -63,7 +77,6 @@ plot.bettr_data <- function(x, odd = c("CF", "OF"), ...) {
         fav_choice == "away_odds" ~ away_odds
       )) |>
       dplyr::select(event_id, fav_choice, opening_fav_odds) -> x_target_odd
-
   }
 
   # Appending original data with this info and transforming data
@@ -75,19 +88,33 @@ plot.bettr_data <- function(x, odd = c("CF", "OF"), ...) {
         fav_choice == "draw_odds" ~ draw_odds,
         fav_choice == "away_odds" ~ away_odds
       ),
-      pct_change_fav_odds = (fav_odds - opening_fav_odds) / opening_fav_odds * 100,
-      tooltip = paste0("event_id: ", event_id)
-    ) |>
-    dplyr::select(event_id, logged_time, pct_change_fav_odds, tooltip) -> x_plottable
-  # Scale and location adjustments are so that we can focus on displaying movement on a similar scale with a fixed start.
-  # A percentage change is a very reasonable scale
+      pct_change_fav_odds = (fav_odds - opening_fav_odds) / opening_fav_odds * 100
+    ) -> x_base
+
+  # Create legend_label and tooltip columns
+  if (is.null(fixture)) {
+    x_base |>
+      dplyr::mutate(
+        legend_label = event_id,
+        tooltip = paste0("event_id: ", event_id)
+      ) -> x_plottable
+  } else {
+    x_base |>
+      dplyr::mutate(
+        legend_label = fixture,
+        tooltip = fixture
+      ) -> x_plottable
+  }
+
+  x_plottable <- x_plottable |>
+    dplyr::select(event_id, legend_label, logged_time, pct_change_fav_odds, tooltip)
 
   p <- ggplot2::ggplot(
     x_plottable,
     ggplot2::aes(
       x = logged_time,
       y = pct_change_fav_odds,
-      colour = event_id,
+      colour = legend_label,
       tooltip = tooltip,
       data_id = event_id
     )
@@ -95,8 +122,11 @@ plot.bettr_data <- function(x, odd = c("CF", "OF"), ...) {
     ggplot2::theme_minimal() +
     ggplot2::xlab("Time") +
     ggplot2::ylab("Odds Change (%)") +
-    ggplot2::ggtitle(paste("Change over time of odds data (", odd, ")")) +
-    ggiraph::geom_line_interactive()
+    ggplot2::ggtitle(ifelse(odd == "CF",
+                            "Change over time of closing favourite odds",
+                            "Change over time of opening favourite odds")) +
+    ggiraph::geom_line_interactive() +
+    ggplot2::scale_color_discrete(name = if (is.null(fixture)) "Event ID" else "Fixture")
 
   ggiraph::girafe(ggobj = p, ...)
 }
