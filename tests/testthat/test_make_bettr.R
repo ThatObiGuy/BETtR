87b295273dde5be05294420c26d149cf33dadc0a
Zala04 <zaladio135@gmail.com>
2026-01-07 15:39:37 +0000

    testthat commit

---
+++ tests/testthat/test_make_bettr.R
@@ -0,0 +1,6 @@
+library(bettr)
+load("~/bettr/data/football.RData")
+test_that("make_bettr converts to  a tsibble", {
+  x <- make_bettr(football, make_tsibble = TRUE)
+  expect_s3_class(x, "tbl_ts")
+})
