c1e03ba9a06e62886d5305f378354f89dc1821e5
Zala <87830639+Zala04@users.noreply.github.com>
2026-01-18 14:40:09 +0000

    Remove loading of football.RData in tests
    make it work

--- tests/testthat/test_make_bettr.R
+++ tests/testthat/test_make_bettr.R
@@ -1,5 +1,4 @@
 library(bettr)
-load("~/bettr/data/football.RData")
 test_that("make_bettr converts to  a tsibble", {
   x <- make_bettr(football, make_tsibble = TRUE)
   expect_s3_class(x, "tbl_ts")
